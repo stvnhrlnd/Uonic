af71c9b84b5ded389d56d1c4fa8b986041c46223
Steven <steven_harland@hotmail.co.uk>
2017-04-23 21:15:29 +0100

    Add UmbracoIdentity

---
+++ Uonic.Umbraco/Models/UmbracoIdentity/UmbracoApplicationRole.cs
@@ -0,0 +1,13 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using UmbracoIdentity;
+using UmbracoIdentity.Models;
+
+namespace Uonic.Umbraco.Models.UmbracoIdentity
+{
+    public class UmbracoApplicationRole : UmbracoIdentityRole
+    {
+
+    }
+}
