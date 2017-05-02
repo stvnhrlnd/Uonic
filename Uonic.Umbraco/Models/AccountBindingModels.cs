2de9729f833a90cd9c117a401075b79f6ba72b8c
Steven <steven_harland@hotmail.co.uk>
2017-05-02 21:56:02 +0100

    Remove confirm password from register model

--- Uonic.Umbraco/Models/AccountBindingModels.cs
+++ Uonic.Umbraco/Models/AccountBindingModels.cs
@@ -51,11 +51,6 @@ namespace Uonic.Umbraco.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 
     public class RegisterExternalBindingModel
