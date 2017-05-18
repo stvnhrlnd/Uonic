0d5cc3b01811fbecd32020622e4eb925581daa5d
Steven Harland <stvnhrlnd@gmail.com>
2017-05-18 23:07:35 +0100

    Remove duplicate code

--- Uonic.Umbraco/App_Start/UmbracoIdentityStartup.cs
+++ Uonic.Umbraco/App_Start/UmbracoIdentityStartup.cs
@@ -75,21 +75,6 @@ namespace Uonic.Umbraco
             // Enable the application to use bearer tokens to authenticate users
             app.UseOAuthBearerTokens(OAuthOptions);
 
-            // Configure the application for OAuth based flow
-            PublicClientId = "self";
-            OAuthOptions = new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                // In production mode set AllowInsecureHttp = false
-                AllowInsecureHttp = true
-            };
-
-            // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
-
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
             //  clientId: "",
