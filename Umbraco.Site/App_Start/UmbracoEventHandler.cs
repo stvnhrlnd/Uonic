8fae88b790846c1f30ca5ff4c86b55939a0e05de
Steven Harland <stvnhrlnd@gmail.com>
2017-06-25 20:32:42 +0100

    Configure Web API to return JSON with camel case property names

---
+++ Umbraco.Site/App_Start/UmbracoEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+using Umbraco.Core;
+
+namespace Umbraco.Site.App_Start
+{
+    public class UmbracoEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            // Configure Web API to return JSON with camel case property names
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+        }
+    }
+}
